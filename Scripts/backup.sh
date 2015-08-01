306b6ccf68cd9d33b77a5142d4b3dc516b98abff
Daniel Sparber <sparber.daniel@gmail.com>
2015-08-01 17:25:40 +0200

    added raspberry pi backup script

---
+++ Scripts/backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+cd /home/
+
+# variables
+backupName="backup"
+packageLists="packageLists"
+
+# current time
+now="$(date +'%Y-%m-%d_%H-%M-%S')"
+echo "Backup process stared: (timestamp: $now):"
+
+# create backup folder
+echo "Copying contents..."
+mkdir $backup
+
+# copying files
+cp -r /etc $backupName
+cp -r $USER $backupName
+
+# creating and copying package list
+mkdir $backupName/$packageLists
+dpkg --get-selections > $backupName/$packageLists/package.list
+cp /etc/apt/sources.list $backupName/$packageLists/sources.list
+apt-key exportall > $backupName/$packageLists/repo.keys
+
+# creating archive (tar.gz)
+echo "Creating archive..."
+tar czf $now.tar.gz $backup
+
+# copying archive to destination device
+echo "Enter the connection information:"
+read -p "IP: " destIP
+read -p "User:" destUser
+read -p "Folder:" destFolder
+
+rsync -ze ssh $now.tar.gz $destUser@$destIP:$destFolder
+echo "Transmitting backup..."
+
+# tidy up
+rm -R $backup
+rm $now.tar.gz
+echo "finished"
+
+# recover:
+#   sudo apt-key add ~/repo.keys
+#   sudo cp ~/sources.list /etc/apt/sources.list
+#   sudo apt-get update
+#   sudo apt-get install dselect
+#   sudo dpkg --set-selections < ~/package.list
+#   sudo dselect
