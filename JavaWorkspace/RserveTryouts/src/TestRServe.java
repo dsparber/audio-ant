c5760a26903443e5cac1b5f231f788e02cd3f81a
Daniel Sparber <sparber.daniel@gmail.com>
2015-10-23 19:26:08 +0200

    Rserve tryouts

---
+++ JavaWorkspace/RserveTryouts/src/TestRServe.java
@@ -0,0 +1,20 @@
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
+
+/**
+ * @author Daniel Sparber
+ * @year 2015
+ *
+ * @version 1.0
+ */
+
+public class TestRServe {
+	public static void main(String[] args) throws RserveException, REXPMismatchException {
+
+		RConnection connection = new RConnection();
+		REXP x = connection.eval("R.version.string");
+		System.out.println(x.asString());
+	}
+}
