6172bf0a9cb32620572fdc8a919d231125687820
Daniel Sparber <sparber.daniel@gmail.com>
2015-11-15 16:55:43 +0100

    implemented Hann windowing

--- JavaWorkspace/AudioAnalysis/src/AudioAnalysis.java
+++ JavaWorkspace/AudioAnalysis/src/AudioAnalysis.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
 
 import wave.WavAudioFileReader;
+import windowing.Windowing;
 
 public class AudioAnalysis {
 
 	public static void main(String[] args) throws IOException {
 
 		Integer[] audioSignal = readAudioFileData("../../Audio Testfiles/Doorbell_d01.wav");
+
+		Integer[][] windows = Windowing.generateHannWindows(audioSignal, 512, 0.5f);
 
 	}
 
