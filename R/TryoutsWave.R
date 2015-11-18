eefaea708ea058be5522ec95e80b5a9774b44f89
Daniel Sparber <sparber.daniel@gmail.com>
2015-11-18 15:14:54 +0100

    R tryout

--- R/TryoutsWave.R
+++ R/TryoutsWave.R
@@ -1,2 +1,9 @@
 library(tuneR)
+library(seewave)
+
 wave = readWave("/Users/daniel/Documents/Arbeiten/Diplomarbeit AudioAnt/Workspace/Audio Testfiles/Doorbell_d01.wav")
+
+resultMFCC = melfcc(wave)
+
+resultZCR = zcr(wave)
+
