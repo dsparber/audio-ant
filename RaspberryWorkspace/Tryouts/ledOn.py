ab62f50f10333d27c0ce95d9be4f6670dd26ab42
Florian Langer <florian.langer2@gmail.com>
2015-11-23 21:02:10 +0100

    added some tryouts and the on-off-functions

--- RaspberryWorkspace/Tryouts/ledOn.py
+++ RaspberryWorkspace/Tryouts/ledOn.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
-GPIO.output(7,True)
+GPIO.setup(40, GPIO.OUT)
+GPIO.output(40,True)
+GPIO.setup(38, GPIO.OUT)
+GPIO.output(38,True)
+GPIO.setup(36, GPIO.OUT)
+GPIO.output(36,True)
