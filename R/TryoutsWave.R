2c6ac32f068c7460ed99e7abcc0d71786025095a
Daniel Sparber <sparber.daniel@gmail.com>
2016-01-07 21:48:49 +0100

    removed r tryouts

--- R/TryoutsWave.R
+++
@@ -1,9 +0,0 @@
-library(tuneR)
-library(seewave)
-
-wave = readWave("/Users/daniel/Documents/Arbeiten/Diplomarbeit AudioAnt/Workspace/Audio Testfiles/Doorbell_d01.wav")
-
-resultMFCC = melfcc(wave)
-
-resultZCR = zcr(wave)
-
