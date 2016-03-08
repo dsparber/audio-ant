ed196b5f3a3d403d2b402e9712d2f1843212092c
Benjamin Hackstock <benni.hack@hotmail.com>
2016-03-08 18:28:57 +0100

    UI design and functionality of AudioAntSettings Activity, extraction of multiple dimensions

--- AndroidWorkspace/AudioAnt/app/src/main/java/diplomarbeit/audioant/Activities/AudioAntSettings.java
+++ AndroidWorkspace/AudioAnt/app/src/main/java/diplomarbeit/audioant/Activities/AudioAntSettings.java
@@ -1,16 +1,33 @@
 package diplomarbeit.audioant.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import diplomarbeit.audioant.Fragments.ShowTextAlert;
 import diplomarbeit.audioant.R;
 
 public class AudioAntSettings extends AppCompatActivity {
 
+    String wlanHelpText;
+    String wlanHelpTextHeader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_ant_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        wlanHelpText = getResources().getString(R.string.wlan_description);
+        wlanHelpTextHeader = getResources().getString(R.string.wlan_description_header);
+
+
+
+    }
+
+    public void showWlanHelp(View v) {
+        ShowTextAlert textAlert = new ShowTextAlert();
+        textAlert.setText(wlanHelpText);
+        textAlert.setHeader(wlanHelpTextHeader);
+        textAlert.show(getFragmentManager(), "wlan help");
     }
 }
