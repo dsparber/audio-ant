2dcc2adb09a33f118e72a937de8977807713152a
Daniel Sparber <sparber.daniel@gmail.com>
2016-03-17 13:52:00 +0100

    Stop hotspot service

--- Scripts/disableHotspot.sh
+++ Scripts/disableHotspot.sh
@@ -3,6 +3,7 @@
 sudo cp /etc/dhcp/dhcpd.conf.bak /etc/dhcp/dhcpd.conf
 sudo cp /etc/default/isc-dhcp-server.bak /etc/default/isc-dhcp-server
 sudo cp /etc/network/interfaces.bak /etc/network/interfaces
+sudo service hostapd stop
 sudo service isc-dhcp-server stop
 sudo ifdown wlan0
 sudo ifup wlan0
